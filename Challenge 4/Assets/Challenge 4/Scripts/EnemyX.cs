94a8295bddd7acd5a15e6ff7e145b8d423930570
Hide Ziegler <hidez246@gmail.com>
2021-11-29 15:38:46 -0700

    fixes for Enemy
    - enemies move towards player goal

--- Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
+++ Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class EnemyX : MonoBehaviour
 {
     public float speed;
-    private Rigidbody enemyRb;
     private GameObject playerGoal;
+    private Rigidbody enemyRb;
+
 
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
     }
 
     // Update is called once per frame
@@ -20,7 +22,6 @@ public class EnemyX : MonoBehaviour
         // Set enemy direction towards player goal and move there
         Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
-
     }
 
     private void OnCollisionEnter(Collision other)
